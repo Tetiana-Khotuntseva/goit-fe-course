1912277946845347f4ffd8af6eecce57a9ceaf07
Tetiana-Khotuntseva <tanya.khotunzeva@gmail.com>
2019-12-05 19:46:29 +0200

    Homework-06

--- javaScript/goit-js-hw-06/task-1.js
+++ javaScript/goit-js-hw-06/task-1.js
@@ -83,3 +83,14 @@ const getNamesSortedByFriendsCount = users =>
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// 10 Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = users =>
+  users
+    .reduce((acc, { skills }) => [...acc, ...skills], [])
+    .filter((skill, index, skills) => skills.indexOf(skill) === index)
+    .sort();
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lore
